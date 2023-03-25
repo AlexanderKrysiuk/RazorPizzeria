8913d866a67ef126015d4ac896e7371989e856fc
Hacker-Wolf <alex.krysiuk@icloud.com>
2023-03-25 14:56:10 +0100

    Setting Pizza Model

--- RazorPizzeria/Pages/Forms/CustomPizza.cshtml.cs
+++ RazorPizzeria/Pages/Forms/CustomPizza.cshtml.cs
@@ -1,9 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using RazorPizzeria.Models;
 namespace RazorPizzeria.Pages.Forms;
 
 public class CustomPizza : PageModel
 {
+    [BindProperty]
+    public PizzasModel Pizza { get; set; }
     public void OnGet()
     {
         
