5584e15466f0a2dad8ad42b1f2798019da32c335
Hacker-Wolf <alex.krysiuk@icloud.com>
2023-03-26 19:13:24 +0200

    Set up a view model

--- RazorPizzeria/Models/PizzasModel.cs
+++ RazorPizzeria/Models/PizzasModel.cs
@@ -2,8 +2,8 @@ namespace RazorPizzeria.Models;
 
 public class PizzasModel
 {
-    public string ImageTitle { get; set; }
-    public string PizzaName { get; set; }
+    public string ImageTitle { get; set; } = null!;
+    public string PizzaName { get; set; } = null!;
     public float BasePrice { get; set; } = 2;
     public bool TomatoSauce { get; set; }
     public bool Cheese { get; set; }
--- RazorPizzeria/Pages/Forms/CustomPizza.cshtml.cs
+++ RazorPizzeria/Pages/Forms/CustomPizza.cshtml.cs
@@ -3,10 +3,9 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using RazorPizzeria.Models;
 namespace RazorPizzeria.Pages.Forms;
 
-public class CustomPizza : PageModel
+public class CustomPizzaModel : PageModel
 {
-    [BindProperty]
-    public PizzasModel Pizza { get; set; }
+    [BindProperty] public PizzasModel Pizza { get; set; } = null!;
     public void OnGet()
     {
         
