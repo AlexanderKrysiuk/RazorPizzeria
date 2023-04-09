0a8651650ae767663ba27afd6a33f0de5c562726
Hacker-Wolf <alex.krysiuk@icloud.com>
2023-04-09 22:32:10 +0200

    Adding an SQL Database

---
+++ RazorPizzeria/Models/PizzaOrder.cs
@@ -0,0 +1,8 @@
+namespace RazorPizzeria.Models;
+
+public class PizzaOrder
+{
+    public int Id { get; set; }
+    public string PizzaName { get; set; }
+    public float BasePrice { get; set; }
+}
--- RazorPizzeria/Program.cs
+++ RazorPizzeria/Program.cs
@@ -1,6 +1,16 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPizzeria.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
