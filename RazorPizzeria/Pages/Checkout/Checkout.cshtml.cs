9a99c9dccc0fd38aaf5566463e92803c02f64ea6
Hacker-Wolf <alex.krysiuk@icloud.com>
2023-04-09 20:19:21 +0200

    OnGet and Displaying data from URL

--- RazorPizzeria/Pages/Checkout/Checkout.cshtml.cs
+++ RazorPizzeria/Pages/Checkout/Checkout.cshtml.cs
@@ -1,11 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorPizzeria.Pages.Checkout;
 
+[BindProperties(SupportsGet = true)]
 public class Checkout : PageModel
 {
+    public string PizzaName { get; set; }
+    public float PizzaPrice { get; set; }
+    public string ImageTitle { get; set; }
     public void OnGet()
     {
-        
+        if (string.IsNullOrWhiteSpace(PizzaName))
+        {
+            PizzaName = "Custom";
+        }
+
+        if (string.IsNullOrWhiteSpace(ImageTitle))
+        {
+            ImageTitle = "Create";
+        }
     }
 }
